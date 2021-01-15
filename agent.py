7381e087dde999655bd0c16b4136c49163620f82
Stephen <stephen@umw.edu>
2021-01-15 10:43:06 -0500

    Make agents move intelligently (rule M).

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from mesa import Agent
 import logging
 from collections import namedtuple
+import operator
 
 EnvSquare = namedtuple('EnvSquare', 'curr max')
 
@@ -25,12 +26,14 @@ class SugarscapeAgent(Agent):
         self.model.scape[dest] = EnvSquare(0, self.model.scape[dest].max)
 
     def _visible_neighbor_with_most_sugar(self):
-        dest = (np.random.choice(range(self.model.grid.width)),
-            np.random.choice(range(self.model.grid.width)))
-        while not self.model.grid.is_cell_empty(dest):
-            dest = (np.random.choice(range(self.model.grid.width)),
-                np.random.choice(range(self.model.grid.width)))
-        return dest
+        nei = { n:self.model.scape[n][0] 
+            for n in self.model.grid.iter_neighborhood(self.pos,
+                moore=False, include_center=False, radius=self.vision) 
+            if (n[0] == self.pos[0] or n[1] == self.pos[1]) 
+                and self.model.grid.is_cell_empty(n) }
+        if len(nei) == 0:
+            return self.pos
+        return max(nei.items(), key=operator.itemgetter(1))[0]
 
     def __str__(self):
         return "Agent {}".format(self.unique_id)
